2cafd9c931ae246a80c12e2841ab6dd9d705d65c
manuelvilla <manuelvillasenor.sa@gmail.com>
2021-03-04 12:50:31 -0600

    =Inicio completo

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 let proximos = [];
 let pasados = [];
+let cargados = 0;
+let divProximos = document.getElementById('proximos');
+let divPasados = document.getElementById('pasados');
 
 $(document).ready(function () {
 
@@ -9,39 +12,45 @@ $(document).ready(function () {
     let eventos = response.eventos;
     let fechaActual = response.fechaActual;
 
-    proximos = eventos.filter(evento => evento.fecha > fechaActual); //Filtrar proximos
+    proximos = eventos.filter(evento => evento.fecha > fechaActual) //Filtrar proximos
     pasados = eventos.filter(evento => evento.fecha < fechaActual); //Filtrar pasados
 
-    console.log(proximos);
-    console.log(pasados);
+    proximos.sort((a,b) => Date.parse(a.fecha) - Date.parse(b.fecha)); //Ordenar próximo
+    pasados.sort((a,b) => Date.parse(b.fecha) - Date.parse(a.fecha)); //Ordenar próximo
 
-    proximos.sort((a,b) => Date.parse(b.fecha) - Date.parse(a.fecha));
+    let dosProximos = proximos.slice(0,2); //2 Eventos próximos
+    let dosPasados = pasados.slice(0,2); //2 Eventos pasados
 
-    console.log(proximos);
-    console.log(pasados);
+    let htmlProximos = ``;
+    let htmlPasados = ``;
 
+    dosProximos.map(evento => {
+      htmlProximos += `<div class="col-sm-6">
+      <div class="card">
+        <div class="card-body">
+          <a href="/detalle.html?id=${evento.id}"><h1>${evento.nombre}</h1></a>
+          <p>${evento.fecha}</p>
+          <p class="card-text">${evento.descripcion}</p>
+        </div>
+      </div>
+      </div>`;
+    });
+
+    dosPasados.map(evento => {
+      htmlPasados += `<div class="col-sm-6">
+      <div class="card">
+        <div class="card-body">
+          <a href="/detalle.html?id=${evento.id}"><h1>${evento.nombre}</h1></a>
+          <p>${evento.fecha}</p>
+          <p class="card-text">${evento.descripcion}</p>
+        </div>
+      </div>
+      </div>`;
+    });
+
+    divProximos.innerHTML = htmlProximos;
+    divPasados.innerHTML = htmlPasados;
+    
   });
-
-  //Clasifica los eventos segun la fecha actual del JSON
-
-  //Ordena los eventos segun la fecha (los mas cercanos primero)
-
-  //Extrae solo dos eventos
-
-  //Ordena los eventos segun la fecha (los mas cercanos primero)
-
-  //Extrae solo dos eventos
-
-  //Crea un string que contenga el HTML que describe el detalle del evento
-
-  //Recorre el arreglo y concatena el HTML para cada evento
-
-  //Modifica el DOM agregando el html generado
-
-  //Crea un string que contenga el HTML que describe el detalle del evento
-
-  //Recorre el arreglo y concatena el HTML para cada evento
-
-  //Modifica el DOM agregando el html generado
 
 });
